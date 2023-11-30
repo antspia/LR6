8a1f561f7f8ab90d75cf57fd56fa5110eb992ea0
antspia <sanya.pohorilii@gmail.com>
2023-11-30 20:57:24 +0200

    s

---
+++ src/test/java/CinemaTest.java
@@ -0,0 +1,32 @@
+import org.example.Cinema;
+
+public class CinemaTest {
+    public static void main(String[] args) {
+        runTests();
+    }
+
+    public static void runTests() {
+        Cinema cinema = new Cinema(5, 10, 20);
+
+        // Test booking seats
+        System.out.println("Test 1: Booking seats");
+        cinema.bookSeats(0, 4, new int[]{2, 3, 4, 5});
+        cinema.printSeatingArrangement(0);
+
+        // Test cancelling booking
+        System.out.println("\nTest 2: Cancelling booking");
+        cinema.cancelBooking(0, 4, new int[]{5});
+        cinema.printSeatingArrangement(0);
+
+        // Test checking availability
+        System.out.println("\nTest 3: Checking availability");
+        boolean availabilityCheck = cinema.checkAvailability(0, 3);
+        System.out.println("Are 3 consecutive seats available? " + availabilityCheck);
+
+        // Test finding best available seats
+        System.out.println("\nTest 4: Finding best available seats");
+        cinema.autoBook(1, 4);
+        cinema.printSeatingArrangement(1);
+
+    }
+}
